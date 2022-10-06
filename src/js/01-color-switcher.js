8c73591adad7eac66e65ec869cc99f85c991cc93
Andrii Sokolovskyi <andrisokolovskiy@gmail.com>
2022-10-06 11:58:30 +0200

    task2 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+let startEnabled = false;
+let intervalId = null;
+
+function onStartClick() {
+  if (startEnabled) {
+    return;
+  } else {
+    intervalId = setInterval(() => {
+      refs.bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+    startEnabled = true;
+  }
+}
+
+function onStopClick() {
+  clearInterval(intervalId);
+  startEnabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
